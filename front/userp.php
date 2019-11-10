3f2b25325e59f3a1321aa5767b018417a94eaa46
Alex0797 <54760155+Alex0797@users.noreply.github.com>
2019-11-10 07:55:52 -0600

    se mira mas bonito asi

--- front/userp.php
+++ front/userp.php
@@ -76,14 +76,14 @@
 							<div class="form-group row">
 								<label class="col-lg-3 col-form-label form-control-label">Especialidad</label>
 								<div class="col-lg-9">
-									<input name="especialidad" class="form-control" type="text" placeholder="Unicamente para doctores">
+									<input name="especialidad" class="form-control" type="text" placeholder="Unicamente para doctores" style="width:200px">
 								</div>
 
                             </div>
                             <div class="form-group row">
 								<label class="col-lg-3 col-form-label form-control-label">Contraseña</label>
 								<div class="col-lg-9">
-									<input name="pass" class="form-control" type="password" placeholder="">
+									<input name="pass" class="form-control" type="password" placeholder="" style="width:200px">
 								</div>
 
                             </div>
@@ -103,8 +103,7 @@
 							<div class="form-group row">
 								<div class="col-lg-12 text-center">
 									<input type="reset" class="btn btn-secondary" value="Cancelar">
-									<input type="submit" class="btn btn-primary"
-										value="Guardar">
+									<input type="submit" class="btn btn-primary" value="Guardar">
 								</div>
 							</div>
 						</form>
